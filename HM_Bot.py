2781d4d61f3f490d8fab9c89798d651112b397ce
Pulviz <101511521+Pulviz@users.noreply.github.com>
2022-09-25 15:35:33 +0300

    Add files via upload

--- HM_Bot.py
+++ HM_Bot.py
@@ -1,7 +1,7 @@
 import os
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
-import bs4
+from bs4 import BeautifulSoup
 import requests
 from datetime import datetime
 
@@ -11,49 +11,33 @@ vk_group_id = 210986020
 vk_session = vk_api.VkApi(token=token)
 vk_session.get_api()
 longpool = VkBotLongPoll(vk_session, vk_group_id)
-commands = ["привет", "дз", "123", "расписание", "очистка", "admin"]
+commands = ["дз", "123", "расписание", "очистка"]
 admins = open("admins.txt").readlines()
 for i in range(len(admins)):
     num = admins[i]
     if "\n" in num:
         admins[i] = num[:-1]
-probably_admins = open("probably_admins.txt").readlines()
-for i in range(len(probably_admins)):
-    num = probably_admins[i]
-    if "\n" in num:
-        if num not in admins:
-            probably_admins[i] = num[:-1]
 timetable = {
-    "русский язык": [1, 4],
-    "алгебра": [2, 3, 4],
-    "физика": [2, 5],
-    "химия": [1, 4],
-    "английский язык": [1, 3, 5],
-    "геометрия": [1, 5],
-    "история": [1, 5],
-    "биология": [1, 5],
-    "литература": [2, 5],
+    "русский язык": [1, 3],
+    "алгебра": [1, 2, 3],
+    "физика": [3, 5],
+    "химия": [1, 3],
+    "английский язык": [2, 4, 5],
+    "геометрия": [1, 2],
+    "история": [1, 2],
+    "биология": [3, 4],
+    "литература": [2, 3, 4],
     "обж": [2],
-    "информатика": [2],
-    "география": [2, 3],
-    "технология отраслей": [3],
-    "культурология": [3],
-    "проектная деятельность": [3],
-    "музыка": [3],
-    "технология": [4],
+    "информатика": [5],
+    "география": [2, 5],
+    "родной русский": [4],
+    "обществознание": [4]
 }
-
-
-def get_name(user):
-    req = requests.get("https://vk.com/id" + str(user))
-    bs = bs4.BeautifulSoup(req.text, "html.parser")
-    user_name = clean_all_tag_from_str(bs.findAll("title")[0])
-    return user_name.split()[0]
 
 
 def get_time():
     req = requests.get("https://my-calend.ru")
-    bs = bs4.BeautifulSoup(req.text, "html.parser")
+    bs = BeautifulSoup(req.text, "html.parser")
     time = clean_all_tag_from_str(str(bs.findAll("p"))).split()[3]
     result = ""
     for c in time:
@@ -91,12 +75,6 @@ def wait(user):
 def admin(user):
     send_msg(user, "Введите название предмета")
     subject = wait(user).object.message['text'].lower()
-    if subject == "английский язык" or subject == "информатика":
-        send_msg(user, "Введите вашу группу")
-        answer = wait(user).object.message['text'].lower()
-        if answer == "1":
-            timetable["английский язык"] = [1, 2, 3]
-            timetable["информатика"] = [5]
     if subject in timetable:
         week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
         today = datetime.now().strftime("%A")
@@ -129,52 +107,30 @@ def read_files(file):
     return result
 
 
-def recruit(user):
-    candidats = probably_admins
-    for i in range(len(candidats)):
-        send_msg(user, f"{i} : {candidats[i]}")
-    answer = wait(user).object.message['text'].lower()
-    if answer != "x":
-        open("admins.txt", "a").write(candidats[int(answer)] + "\n")
-        with open("probably_admins.txt", "w") as f:
-            for c in candidats:
-                if c != candidats[int(answer)]:
-                    f.write(c)
-
-
 def new_message(message, user):
-    if message.lower() == commands[0]:                                  #Привет
-        return f"Привет, {get_name(user)}!"
-    elif message.lower() == commands[1]:                                #ДЗ
-        i = 1
+    if message.lower() == commands[0]:
+        days_to = 1
         if datetime.now().strftime("%A") == "Friday":
-            i = 3
+            days_to = 3
         elif datetime.now().strftime("%A") == "Saturday":
-            i = 2
-        if os.path.isfile(f"days/{str(get_time() + i)}.txt"):
-            return read_files(f"days/{str(get_time() + i)}")
+            days_to = 2
+        if os.path.isfile(f"days/{str(get_time() + days_to)}.txt"):
+            return read_files(f"days/{str(get_time() + days_to)}")
         else:
             return "Ничего не задано"
-    elif message.lower() == commands[2]:                                #123
+    elif message.lower() == commands[1]:
         if str(user) in admins:
             admin(user)
             return "Подтвержденно"
-        else:
-            if str(user) not in probably_admins:
-                open("probably_admins.txt", "a").write(str(user) + "\n")
-            return "Ожидайте подтверждения"
-    elif message.lower() == commands[3]:                                #Расписание
+    elif message.lower() == commands[2]:
         return read_files("TT")
-    elif message.lower() == commands[4]:                                #Очистка
+    elif message.lower() == commands[3]:
         files = os.listdir("days")
         today = get_time()
         for c in files:
             if int(c[:-4]) < today:
                 os.remove("days/" + c)
         return "Очистка завершена"
-    elif message.lower() == commands[5] and user == 586097800:          #admin
-        recruit(user)
-        return "Подтверждено"
     else:
         return "???"
 
